de5922a82619a1acb5648bc3ad32661dc84bd912
LIMSUNGSAN <lkt10004@gmail.com>
2025-05-30 16:31:18 +0900

    feat : 관리자 대시보드 제작

--- src/com/team1/dto/AdminMainDTO.java
+++ src/com/team1/dto/AdminMainDTO.java
@@ -1,65 +1,73 @@
 package com.team1.dto;
 
+import java.util.List;
+
 public class AdminMainDTO
 {
+	// 일일 회원가입 등록일, 돌봄 등록일
 	private String reg_date, req_date;
 	
-	private int countReg, countGenReq, countEmgReq;
+	// 회원가입 수, 일반돌봄 예약 수
+	private int countReg, countDayGenReq;
+	
+	// 월별 예약
+	private List<String> monthLabels;       
+	private List<Integer> countMonthGenReq;
 	
 	// getter / setter 구성
-	public int getCountReg()
-	{
-		return countReg;
-	}
-
-	public void setCountReg(int countReg)
-	{
-		this.countReg = countReg;
-	}
-
-	public int getCountGenReq()
-	{
-		return countGenReq;
-	}
-
-	public void setCountGenReq(int countGenReq)
-	{
-		this.countGenReq = countGenReq;
-	}
-
-	public int getCountEmgReq()
-	{
-		return countEmgReq;
-	}
-
-	public void setCountEmgReq(int countEmgReq)
-	{
-		this.countEmgReq = countEmgReq;
-	}
-
 	public String getReg_date()
 	{
 		return reg_date;
 	}
-
 	public void setReg_date(String reg_date)
 	{
 		this.reg_date = reg_date;
 	}
-
 	public String getReq_date()
 	{
 		return req_date;
 	}
-
 	public void setReq_date(String req_date)
 	{
 		this.req_date = req_date;
 	}
-	
-	
+	public int getCountReg()
+	{
+		return countReg;
+	}
+	public void setCountReg(int countReg)
+	{
+		this.countReg = countReg;
+	}
+	public int getCountDayGenReq()
+	{
+		return countDayGenReq;
+	}
+	public void setCountDayGenReq(int countDayGenReq)
+	{
+		this.countDayGenReq = countDayGenReq;
+	}
+	public List<String> getMonthLabels()
+	{
+		return monthLabels;
+	}
+	public void setMonthLabels(List<String> monthLabels)
+	{
+		this.monthLabels = monthLabels;
+	}
+	public List<Integer> getCountMonthGenReq()
+	{
+		return countMonthGenReq;
+	}
+	public void setCountMonthGenReq(List<Integer> countMonthGenReq)
+	{
+		this.countMonthGenReq = countMonthGenReq;
+	}
 	
 
+	
+	
+	
 	
 	
 	
--- src/com/team1/mybatis/IAdminMainDAO.java
+++ src/com/team1/mybatis/IAdminMainDAO.java
@@ -1,24 +1,29 @@
 package com.team1.mybatis;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import com.team1.dto.AdminDTO;
 
 public interface IAdminMainDAO
 {
-	// 관리자 정보 조회
-	public ArrayList<AdminDTO> list();
+	  // 관리자 관련
+    public ArrayList<AdminDTO> list();
+    public int update(AdminDTO admin);
+    public AdminDTO search(String admin_reg_id);
+
+    // 통계 관련
+    public int countReg();
+    public int countDayGenReq();
+    
+    // 월간 시터/부모 회원가입 수
+    public int countMonthParReg();
+    public int countMonthSitReg();
+
+    // 월별 일반 예약 건수
+    public List<Map<String, Object>> countMonthGenReq();
 	
-	// 관리자 정보 수정
-	public int update(AdminDTO admin);
-	
-	// 관리자 찾기
-	public AdminDTO search(String admin_reg_id);
-	
-	// 회원가입 요청 수
-	public int countReg();
-	
-	//  일일 일반돌봄 예약 수 
-	public int countGenReq();
-	
+    // 월간 매출액
+    public int MonthPayment();
 }
